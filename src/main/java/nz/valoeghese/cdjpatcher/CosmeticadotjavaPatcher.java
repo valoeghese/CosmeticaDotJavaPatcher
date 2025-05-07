951e59393e66e4005579ffa0634e6d03cca4ef63
valoeghese <46117728+valoeghese@users.noreply.github.com>
2025-05-07 16:33:07 +1200

    fix: set environment to client

--- src/main/java/nz/valoeghese/cdjpatcher/CosmeticadotjavaPatcher.java
+++ src/main/java/nz/valoeghese/cdjpatcher/CosmeticadotjavaPatcher.java
@@ -2,16 +2,16 @@ package nz.valoeghese.cdjpatcher;
 
 import cc.cosmetica.api.CosmeticaAPI;
 import cc.cosmetica.api.LoreType;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public class CosmeticadotjavaPatcher implements ModInitializer {
+public class CosmeticadotjavaPatcher implements ClientModInitializer {
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		// e.g. print lores (for testing)
 		// also print the message to check it changed
 		if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
